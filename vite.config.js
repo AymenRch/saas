aa109820a8aed4e50178f474a3195a58ab883ede
AymenRch <aymenrch37@gmail.com>
2026-04-15 18:00:09 +0200

    Change base path in Vite config to root

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/saas/',
+  base: '/',
   server: {
   allowedHosts: ['.trycloudflare.com'],
 }
